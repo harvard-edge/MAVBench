51d6344e2ca30e76bf58a0fbf99fee197e9b9bab
Hasan Genc <hngenc@utexas.edu>
2017-11-08 17:49:42 -0600

    Updated README and install scripts

--- prereqs.sh
+++ prereqs.sh
@@ -1,5 +1,12 @@
 cd ..
 
+# ROS packages
+sudo apt-get install ros-kinetic-octomap*
+sudo apt-get install ros-kinetic-depth-image-proc
+sudo apt-get install ros-kinetic-stereo-msgs
+sudo apt-get install ros-kinetic-tf-conversions
+sudo apt-get install ros-kinetic-rviz-visual-tools
+
 # Prereqs for mav_trajectory_generation, our trajectory-smoothening package
 git clone https://github.com/catkin/catkin_simple.git
 git clone https://github.com/ethz-asl/eigen_catkin.git
@@ -8,6 +15,12 @@ git clone https://github.com/ethz-asl/glog_catkin.git
 git clone https://github.com/ethz-asl/mav_comm.git
 git clone https://github.com/ethz-asl/nlopt.git
 git clone https://github.com/ethz-asl/mav_trajectory_generation.git
+
+# Prereqs for SAR
+git clone https://github.com/ethz-asl/volumetric_mapping.git
+git clone https://github.com/ethz-asl/gflags_catkin.git
+git clone https://github.com/ethz-asl/minkindr.git
+git clone https://github.com/ethz-asl/minkindr_ros.git
 
 # Package to publish Airsim imgs and camera info
 git clone https://github.com/marcelinomalmeidan/publishAirsimImgs.git
