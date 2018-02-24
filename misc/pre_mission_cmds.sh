15b96bdb25fd0cc8aba0382f0ed44cc348299d45
Wenzhi Cui <cwz920716@gmail.com>
2018-02-24 00:59:54 -0600

    stuff changed

--- misc/pre_mission_cmds.sh
+++ misc/pre_mission_cmds.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
-sleep 10
-#echo "a"
-echo "f 0 0 1 3"
-#echo "r"
-#sleep 6
-# echo "f 0 0 -1 2"
-# sleep 1
-# echo "f 0 0 -1 2"
-# sleep 2
-# echo "f 0 0 -1 4"
-# sleep 5
-# echo "f 0 0 -0.5 1.5"
-# sleep 1.5
-#kill -INT $(ps aux | grep ros | awk '{print $2}')
-sleep 4
-echo "c"
-#sleep 30
-#sleep 15
-# echo "40 -10 -3"
-echo "10 10 3"
+echo f 0 0 1 3
+echo s
+echo c
+
--- run_time/supervisor.py
+++ run_time/supervisor.py
@@ -2,7 +2,7 @@ import os
 from time import *
 import sys
 import subprocess
-
+import signal
 def action_upon_termination():
     ctr = 0
     ros_node_pid_list = []
@@ -11,17 +11,25 @@ def action_upon_termination():
     while(True): #repeately ask for killing of the processes
         ros_process_list = (subprocess.check_output("ps aux | grep ros |grep log", shell=True)).splitlines()
         ros_process_list_filtered = ros_process_list[:]
-        
+       
+	
+ 
         for process in ros_process_list:  #filter the list
             for ignore_el in process_ignore_list:
                 if ignore_el in process:
                     ros_process_list_filtered.remove(process)
                     break
 
+
         # block till all processes except the ones in the process ignore_list are killed
         for process in ros_process_list_filtered:
+	    if "grep" in process.split():
+		continue	
             pid = process.split()[1] 
-            subprocess.Popen("kill -INT $(ps aux | grep "+ pid + " | awk '{print $2}')", shell=True)
+            print pid 
+	    #os.kill(int(pid), 9) 
+	    #subprocess.Popen("kill -INT $(ps aux | grep "+ pid + " | awk '{print $2}')", shell=True)
+	    subprocess.Popen("kill -INT  "+ pid, shell=True)
             sleep(.3) 
         print process 
         sleep(3) 
@@ -32,7 +40,7 @@ def action_upon_termination():
             if (ctr > 5): #a time out
                 break
         ctr+=1
-
+#    sys.exit(0)
     #ros_process_list_filtered = (subprocess.check_output(['rosnode', 'list'])).splitlines()
     #time.sleep(2) 
     #if (ctr > 5): #a time out
@@ -40,11 +48,21 @@ def action_upon_termination():
     
     # to kill all other processes (including the rosmaster and core and rosout), the
     # easiest way is to kill the roslaunch itself
-    roslaunch_process = (subprocess.check_output("ps aux | grep roslaunch |grep opt", shell=True)).splitlines()[0]
-    pid = roslaunch_process.split()[1] 
-    #--- using call cause it's blocking 
-    subprocess.call("kill -INT $(ps aux | grep "+ pid + " | awk '{print $2}')", shell=True)
+    roslaunch_process = (subprocess.check_output("ps aux | grep roslaunch |grep opt", shell=True)).splitlines()
+    for el in roslaunch_process:
+    	if "grep" in el:
+		continue 
+	else:
+		pid = el.split()[1] 
+    		subprocess.Popen("kill -INT  "+ pid, shell=True)
 
+    #--- using call cause it's blocking 
+    #subprocess.call("kill -INT $(ps aux | grep "+ pid + " | awk '{print $2}')", shell=True
+#print roslaunch_process 
+    #sye.exit(0)
+    #print pid 
+    #os.kill(int(pid), 9) 
+	     #subprocess.Popen("sudo kill -INT $(ps aux | grep "+ pid + " | awk '{print $2}')", shell=True)
 def should_terminate(stat_file):
     try:
         stat_f_hndlr = open(stat_file, "r")
@@ -65,21 +83,27 @@ def should_terminate(stat_file):
 
 def main():
     # for debugging 
+     
+    
+    """
+    # stat_file = "../data/"+"mapping"+"/supervisor_mailbox.txt"
+    stat_file = "/home/wcui/catkin_ws/src/mav-bench/data/mapping/supervisor_mailbox.txt"
+    max_run_time = 90000 
     """ 
-    stat_file = "../data/"+"mapping"+"/supervisor_mailbox.txt"
-    max_run_time = 1000000 
+
     polling_freq = 5  #polling 
-    """ 
+     
     assert(len(sys.argv) == 4)
     mav_bench_dir = sys.argv[1]
     app =  sys.argv[2]
     max_run_time= sys.argv[3]
+ 
     stat_file = mav_bench_dir+"/data/"+app+"/supervisor_mailbox.txt"
-      
     stat_f_hndlr = open(stat_file, "w")
     stat_f_hndlr.close()
     initial_time = time() 
-    
+    #print stat_file
+    #sys.exit(0) 
     #---- body
     while(True): 
         sleep(float(polling_freq))
@@ -91,3 +115,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
