8c65fd1123d86bd42c82ad3b67c8ba69c0fbc217
Hasan Genc <hngenc@utexas.edu>
2018-05-23 21:34:48 -0500

    Added new future_collision class as a seperate library

---
+++ common/future_collision/include/future_collision/future_collision.h
@@ -0,0 +1,98 @@
+#ifndef MAVBENCH_FUTURE_COLLISION_H
+#define MAVBENCH_FUTURE_COLLISION_H
+
+#include "ros/ros.h"
+
+// Standard headers
+#include <string>
+#include <utility>
+
+// Octomap specific headers
+#include <octomap/octomap.h>
+#include <octomap/OcTree.h>
+
+// MAVBench headers
+#include "Drone.h"
+#include "common.h"
+#include "timer.h"
+#include <mavbench_msgs/multiDOFtrajectory.h>
+#include <mavbench_msgs/future_collision.h>
+
+// Octomap server headers. This is only needed for profiling services
+// TODO: Get rid of this dependency
+#include <octomap_server/OctomapServer.h>
+
+class FutureCollisionChecker {
+public:
+    FutureCollisionChecker(octomap::OcTree * octree_) :
+        nh("~"),
+        octree(octree_),
+        // The initialization below looks quite strange, but its actually
+        // really simple. We use C++'s comma operator to call
+        // "future_collision_initialize_params()", which initializes
+        // "ip_addr__global" and some other arguments, before calling the
+        // "Drone" constructor.
+        drone((future_collision_initialize_params(), ip_addr__global.c_str()),
+            port, localization_method)
+    {
+        nh.subscribe<mavbench_msgs::multiDOFtrajectory>("/next_steps", 1, &FutureCollisionChecker::pull_traj, this);
+        col_coming_pub = nh.advertise<mavbench_msgs::future_collision>("/col_coming", 1);
+    }
+
+    void run();
+    void log_data_before_shutting_down();
+
+    // TODO: Get rid of this function
+    void setOctomapServer(octomap_server::OctomapServer * server_)
+    {
+        server = server_;
+    }
+
+private:
+    std::pair <bool,double> check_for_collisions(Drone& drone);
+    bool collision(const octomap::OcTree * octree, const multiDOFpoint& n1, const multiDOFpoint& n2) const;
+    void pull_traj(const mavbench_msgs::multiDOFtrajectory::ConstPtr& msg);
+    void future_collision_initialize_params();
+
+private:
+    ros::NodeHandle nh;
+    ros::Publisher col_coming_pub;
+
+    Drone drone;
+    uint16_t port = 41451;
+
+    int future_collision_seq_id = 0;
+    const octomap::OcTree * octree = nullptr;
+    trajectory_t traj;
+    int traj_future_collision_seq_id = 0;
+
+    // Parameters
+    std::string ip_addr__global;
+    std::string localization_method;
+    double drone_height__global;
+    double drone_radius__global;
+
+    // Profiling variables
+    const octomap_server::OctomapServer * server = nullptr; // TODO: get rid of this dependency
+    ros::Time start_hook_chk_col_t, end_hook_chk_col_t;                                          
+    long long g_checking_collision_kernel_acc = 0;
+    ros::Time g_checking_collision_t;
+    long long g_future_collision_main_loop = 0;
+    int g_check_collision_ctr = 0;
+    double g_distance_to_collision_first_realized = 0;
+    bool CLCT_DATA = false;
+    bool DEBUG = false;
+    ros::Time g_pt_cloud_header; //this is used to figure out the octomap msg that 
+                              //collision was detected in
+    long long g_pt_cloud_future_collision_acc = 0;
+    int g_octomap_rcv_ctr = 0;
+    ros::Duration g_pt_cloud_to_future_collision_t; 
+    int g_main_loop_ctr = 0;
+    long long g_accumulate_loop_time = 0; //it is in ms
+    long long g_pt_cld_to_octomap_commun_olverhead_acc = 0;
+    long long octomap_integration_acc = 0;
+    int octomap_ctr = 0;
+};
+
+#endif
+
---
+++ common/future_collision/src/future_collision.cpp
@@ -0,0 +1,206 @@
+#include <future_collision/future_collision.h>
+
+// Standard headers
+#include <tuple>
+#include <cmath>
+
+// MAVBench headers
+#include <profile_manager/profiling_data_srv.h>
+#include <profile_manager/start_profiling_srv.h>
+
+
+bool FutureCollisionChecker::collision(const octomap::OcTree * octree, const multiDOFpoint& n1, const multiDOFpoint& n2) const
+{
+    const double height = drone_height__global; 
+    const double radius = drone_radius__global; 
+
+    // Create a bounding box representing the drone
+    octomap::point3d min(n1.x-radius, n1.y-radius, n1.z-height/2);
+    octomap::point3d max(n1.x+radius, n1.y+radius, n1.z+height/2);
+
+    // Create a direction vector over which to check for collisions
+    double dx = n2.x - n1.x;
+    double dy = n2.y - n1.y;
+    double dz = n2.z - n1.z;
+    double distance = std::sqrt(dx*dx + dy*dy + dz*dz);
+    octomap::point3d direction(dx, dy, dz);
+
+    for (auto it = octree->begin_leafs_bbx(min, max),
+            end = octree->end_leafs_bbx(); it != end; ++it)
+    {
+        octomap::point3d start_point (it.getCoordinate());
+        octomap::point3d end_point;
+        if (octree->castRay(start_point, direction, end_point, true, distance))
+            return true;
+    }
+
+    return false;
+}
+
+void FutureCollisionChecker::pull_traj(const mavbench_msgs::multiDOFtrajectory::ConstPtr& msg)
+{
+    traj_future_collision_seq_id = msg->future_collision_seq;
+
+    auto pos = drone.position();
+    const auto& traj_front = msg->points.front();
+    double x_offset = pos.x - traj_front.x;
+    double y_offset = pos.y - traj_front.y;
+    double z_offset = pos.z - traj_front.z;
+
+    traj = create_trajectory_from_msg(*msg);
+    for (auto& point : traj) {
+        point.x += x_offset;
+        point.y += y_offset;
+        point.z += z_offset;
+    }
+}
+
+std::pair <bool,double> FutureCollisionChecker::check_for_collisions(Drone& drone)
+{
+    start_hook_chk_col_t = ros::Time::now();
+
+    //RESET_TIMER();
+
+    if (octree == nullptr || traj.empty())
+        return std::make_pair(false, 0.0);
+
+    bool col = false;
+    double time_to_collision = 0.0;
+
+    for (int i = 0; i < traj.size() - 1; ++i) {
+        auto& pos1 = traj[i];
+        auto& pos2 = traj[i+1];
+
+        if (collision(octree, pos1, pos2)) {
+            col = true;
+
+            if(CLCT_DATA) {
+                // g_distance_to_collision_first_realized = dist_to_collision(drone, pos1);
+                g_distance_to_collision_first_realized = time_to_collision;
+            }
+
+            break;
+        }
+
+        time_to_collision += pos1.duration;
+    }
+
+    //LOG_ELAPSED(future_collision);
+    
+    end_hook_chk_col_t = ros::Time::now(); 
+    g_checking_collision_t = end_hook_chk_col_t;
+    g_checking_collision_kernel_acc += ((end_hook_chk_col_t - start_hook_chk_col_t).toSec()*1e9);
+    g_check_collision_ctr++;
+    return std::make_pair(col, time_to_collision);
+}
+
+
+void FutureCollisionChecker::future_collision_initialize_params()
+{
+    ros::param::get("/future_col_drone_radius", drone_radius__global);
+    ros::param::get("/future_col_drone_height", drone_height__global);
+
+    if(!ros::param::get("/package_delivery/ip_addr",ip_addr__global)) {
+        ROS_FATAL("Could not start exploration. IP address parameter missing!");
+        return;
+    }
+    if(!ros::param::get("/package_delivery/localization_method",localization_method)) {
+        ROS_FATAL("Could not start exploration. Localization parameter missing!");
+        return; 
+    }
+	if(!ros::param::get("/CLCT_DATA",CLCT_DATA)) {
+        ROS_FATAL("Could not start exploration. Localization parameter missing!");
+        return; 
+    }
+    if(!ros::param::get("/DEBUG",DEBUG)) {
+        ROS_FATAL("Could not start exploration. Localization parameter missing!");
+        return; 
+    }
+}
+
+void FutureCollisionChecker::log_data_before_shutting_down()
+{
+    std::string ns = ros::this_node::getName();
+    profile_manager::profiling_data_srv profiling_data_srv_inst;
+
+    profiling_data_srv_inst.request.key = "future_collision_kernel";
+    profiling_data_srv_inst.request.value = (((double)g_checking_collision_kernel_acc)/1e9)/g_check_collision_ctr;
+    if (ros::service::waitForService("/record_profiling_data", 10)){ 
+        if(!ros::service::call("/record_profiling_data",profiling_data_srv_inst)){
+            ROS_ERROR_STREAM("could not probe data using stats manager");
+            ros::shutdown();
+        }
+    }
+
+    profiling_data_srv_inst.request.key = "future_collision_main_loop";
+    profiling_data_srv_inst.request.value = (((double)g_future_collision_main_loop)/1e9)/g_check_collision_ctr;
+    if (ros::service::waitForService("/record_profiling_data", 10)){ 
+        if(!ros::service::call("/record_profiling_data",profiling_data_srv_inst)){
+            ROS_ERROR_STREAM("could not probe data using stats manager");
+            ros::shutdown();
+        }
+    }
+
+    profiling_data_srv_inst.request.key = "img_to_octomap_commun_t";
+    profiling_data_srv_inst.request.value = ((double)g_pt_cld_to_octomap_commun_olverhead_acc/1e9)/octomap_ctr;
+    if (ros::service::waitForService("/record_profiling_data", 10)){ 
+        if(!ros::service::call("/record_profiling_data",profiling_data_srv_inst)){
+            ROS_ERROR_STREAM("could not probe data using stats manager using octomap");
+            ros::shutdown();
+        }
+    }
+
+    profiling_data_srv_inst.request.key = "octomap_integration";
+    profiling_data_srv_inst.request.value = (((double)octomap_integration_acc)/1e9)/octomap_ctr;
+    if (ros::service::waitForService("/record_profiling_data", 10)){ 
+        if(!ros::service::call("/record_profiling_data",profiling_data_srv_inst)){
+            ROS_ERROR_STREAM("could not probe data using stats manager using octomap");
+            ros::shutdown();
+        }
+    }
+
+    ROS_INFO_STREAM("done with the octomap profiles");
+}
+
+void FutureCollisionChecker::run()
+{
+    static ros::Time main_loop_start_hook_t;
+    static ros::Time main_loop_end_hook_t;
+
+    main_loop_start_hook_t = ros::Time::now();
+
+    if (CLCT_DATA){ 
+        g_pt_cloud_header = server->rcvd_point_cld_time_stamp; 
+        octomap_ctr = server->octomap_ctr;
+        octomap_integration_acc = server->octomap_integration_acc; 
+        g_pt_cld_to_octomap_commun_olverhead_acc = server->pt_cld_octomap_commun_overhead_acc;
+    }
+
+    if (traj_future_collision_seq_id >= future_collision_seq_id) {
+        bool collision_coming;
+        double time_to_collision;
+        std::tie(collision_coming, time_to_collision) = check_for_collisions(drone);
+
+        if (collision_coming) {
+            future_collision_seq_id++;
+
+            mavbench_msgs::future_collision col_coming_msg;
+            col_coming_msg.header.stamp = g_pt_cloud_header;
+            col_coming_msg.header.seq = future_collision_seq_id;
+            col_coming_msg.collision = collision_coming;
+            col_coming_msg.time_to_collision = time_to_collision;
+
+            col_coming_pub.publish(col_coming_msg);
+
+            // Profiling 
+            if(CLCT_DATA)
+                g_pt_cloud_to_future_collision_t = start_hook_chk_col_t - g_pt_cloud_header;
+            if(DEBUG)
+                ROS_INFO_STREAM("pt cloud to start of checking collision in future collision"<< g_pt_cloud_to_future_collision_t);
+        }
+    }
+
+    main_loop_end_hook_t = ros::Time::now();
+    g_future_collision_main_loop += (main_loop_end_hook_t - main_loop_start_hook_t).toSec()*1e9; 
+}
+
