4bc182af83c8f7ce8a25f31bf545b3b7a1db5a38
zaddan <behzadboro@gmail.com>
2018-09-20 20:25:22 -0700

    some changes

--- prereqs.sh
+++ prereqs.sh
@@ -1,13 +1,13 @@
 cd ..
 
 # ROS packages
-sudo apt-get install ros-kinetic-octomap*
-sudo apt-get install ros-kinetic-depth-image-proc
-sudo apt-get install ros-kinetic-stereo-msgs
-sudo apt-get install ros-kinetic-tf-conversions
-sudo apt-get install ros-kinetic-rviz-visual-tools
-sudo apt-get install ros-kinetic-vision-opencv
-sudo apt-get install ros-kinetic-ompl
+sudo apt-get install -y ros-kinetic-octomap*
+sudo apt-get install -y ros-kinetic-depth-image-proc
+sudo apt-get install -y ros-kinetic-stereo-msgs
+sudo apt-get install -y ros-kinetic-tf-conversions
+sudo apt-get install -y ros-kinetic-rviz-visual-tools
+sudo apt-get install -y ros-kinetic-vision-opencv
+sudo apt-get install -y ros-kinetic-ompl
 
 # Pre-reqs to side-step PCL C++11 issue
 git clone https://github.com/ros-perception/perception_pcl.git
@@ -22,12 +22,11 @@ git clone https://github.com/ethz-asl/nlopt.git
 git clone https://github.com/ethz-asl/mav_trajectory_generation.git
 
 # Prereqs for SAR
-git clone --recursive https://github.com/ethz-asl/nbvplanner.git
+#git clone --recursive https://github.com/ethz-asl/nbvplanner.git
 #git clone https://github.com/ethz-asl/volumetric_mapping.git
 #git clone https://github.com/ethz-asl/gflags_catkin.git
 #git clone https://github.com/ethz-asl/minkindr.git
 #git clone https://github.com/ethz-asl/minkindr_ros.git
-rm -r nbvplanner/nbvplanner
 # Package to publish Airsim imgs and camera info
 git clone https://github.com/marcelinomalmeidan/publishAirsimImgs.git
 
