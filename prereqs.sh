fb008cc2e7158c326f24349ebf76af0af10871d9
zaddan <behzadboro@gmail.com>
2018-09-21 17:39:44 -0700

    prereqs.sh updated

--- prereqs.sh
+++ prereqs.sh
@@ -13,20 +13,20 @@ sudo apt-get install -y ros-kinetic-ompl
 git clone https://github.com/ros-perception/perception_pcl.git
 
 # Prereqs for mav_trajectory_generation, our trajectory-smoothening package
-#git clone https://github.com/catkin/catkin_simple.git
-#git clone https://github.com/ethz-asl/eigen_catkin.git
-#git clone https://github.com/ethz-asl/eigen_checks.git
-#git clone https://github.com/ethz-asl/glog_catkin.git
-#git clone https://github.com/ethz-asl/mav_comm.git
+git clone https://github.com/catkin/catkin_simple.git
+git clone https://github.com/ethz-asl/eigen_catkin.git
+git clone https://github.com/ethz-asl/eigen_checks.git
+git clone https://github.com/ethz-asl/glog_catkin.git
+git clone https://github.com/ethz-asl/mav_comm.git
 git clone https://github.com/ethz-asl/nlopt.git
 git clone https://github.com/ethz-asl/mav_trajectory_generation.git
 
 # Prereqs for SAR
 #git clone --recursive https://github.com/ethz-asl/nbvplanner.git
 #git clone https://github.com/ethz-asl/volumetric_mapping.git
-#git clone https://github.com/ethz-asl/gflags_catkin.git
-#git clone https://github.com/ethz-asl/minkindr.git
-#git clone https://github.com/ethz-asl/minkindr_ros.git
+git clone https://github.com/ethz-asl/gflags_catkin.git
+git clone https://github.com/ethz-asl/minkindr.git
+git clone https://github.com/ethz-asl/minkindr_ros.git
 # Package to publish Airsim imgs and camera info
 git clone https://github.com/marcelinomalmeidan/publishAirsimImgs.git
 
