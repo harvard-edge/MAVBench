acffdc9116c3013937e2c8a0f6aceaacb7a09575
zaddan <behzadboro@gmail.com>
2018-10-11 03:14:21 +0000

    root_setup.bash cleaned up for the new submodule way of doing things

--- build-scripts/root_setup.bash
+++ build-scripts/root_setup.bash
@@ -2,14 +2,13 @@
 
 env_dir="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd))"
 source ${env_dir}/build-scripts/setup_env_var.sh
-
 set -x 
 set -e 
 
-# should remove so if the previous runs have failed, it woulnd't cause an 
+# should remove so if the previous runs have failed, so that it woulnd't cause an 
 # issue
 rm -f /etc/apt/sources.list.d/ros-latest.list # if we don't remove, we can't be 
-                                              # sure which stage last tim
+                                              # sure in which stage terminated last time
 
 #--- update/upgrade and install relevant packages
 echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list
@@ -20,13 +19,14 @@ apt-get update
 ##--- install ros-kinetic-desktop-full
 apt-get install -y ros-kinetic-desktop-full ros-kinetic-rviz-visual-tools ros-kinetic-ompl
 
-mkdir -p $base_dir/build
 
-# intall ROS OpenCV
-if [[ ! -d "$base_dir/build/opencv" ]];then
-	cd $base_dir && \
-		git clone -b 3.1.0-with-cuda8 https://github.com/daveselinger/opencv opencv
-fi
+############
+# ROS OpenCV (installation)
+############
+#if [[ ! -d "$base_dir/src/opencv" ]];then
+#    git clone -b 3.1.0-with-cuda8 https://github.com/daveselinger/opencv ${base_dir}/src/opencv
+#fi
+#
 
 # the following needs to change so that it's not specific to a version of opencv
 # (if we want to stick to a version we need to provide the source code ourselves
@@ -43,9 +43,8 @@ if [[ ! -e "ros-kinetic-opencv3_3.3.1-5xenial_arm64.deb" ]];then
     sed -i 's/\(\bdh_shlibdeps.*\)$/\1 --dpkg-shlibdeps-params=--ignore-missing-info/' debian/rules || exit 1
 	dpkg-buildpackage -b -uc 
 fi
-
  
-if [[ ! -e $base_dir/build-deps/ros_install_done.txt ]]; then
+if [[ ! -e $base_dir/src/ros_opencv_install_done.txt ]]; then
     rm -rf /usr/src/deb_mavbench 
     mkdir /usr/src/deb_mavbench
 	cp $base_dir/src/ros-kinetic-opencv3_3.3.1-5xenial_arm64.deb /usr/src/deb_mavbench/
@@ -63,24 +62,27 @@ if [[ ! -e $base_dir/build-deps/ros_install_done.txt ]]; then
 	apt-get update && \
 	apt-get install -y ros-kinetic-desktop-full ros-kinetic-rviz-visual-tools ros-kinetic-octomap* ros-kinetic-ompl
     cp /opt/ros/kinetic/lib/aarch64-linux-gnu/pkgconfig/opencv-3.3.1-dev.pc /opt/ros/kinetic/lib/aarch64-linux-gnu/pkgconfig/opencv.pc 
-    cd $base_dir/build
-    echo "done" > ros_install_done.txt
+    cd $base_dir/src
+    echo "done" > ros_opencv_install_done.txt
 fi
 
-#--- point cloud library
-cd $base_dir/src
-if [[ ! -d "pcl" ]]; then
-    cd $base_dir/src && git clone https://github.com/PointCloudLibrary/pcl.git &&\
-    cd pcl && git checkout pcl-1.7.2rc2.1
-fi
 
+############
+# point cloud library
+###########
+#cd $base_dir/src
+#if [[ ! -d "pcl" ]]; then
+#    cd $base_dir/src && git clone https://github.com/PointCloudLibrary/pcl.git &&\
+#    cd pcl && git checkout pcl-1.7.2rc2.1
+#fi
+#
 
 # for some reason this is not necessary in the docker (but otherwise pcl
 # is gonna issue an error
 cd /usr/lib/aarch64-linux-gnu/
 sudo ln -sf tegra/libGL.so libGL.so
 
-cd $mavbench_base_dir/pcl
+cd $base_dir/src/pcl
 if [[ ! `git status --porcelain`  ]]; then
     cp $base_dir/build-scripts/lzf_image_io.cpp $base_dir/src/pcl/io/src/ 
 fi
@@ -89,20 +91,15 @@ cd $base_dir/src/pcl && mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=R
 cd $base_dir/src/pcl/build && make -j 4
 cd $base_dir/src/pcl/build && make -j 4 install
 
+
+########
+# AirSim 
+########
+# Note: needs to run with root privilege cause it "apt-get install build-essentials"
 # TODO: this is gonna cause linker for libs that use pcl to run everytime
 #       somehow, we need to figure out how to avoid doing it everytime
-cd $mavbench_base_dir/build-deps && chmod +x relocate_pcl.sh && ./relocate_pcl.sh
+cd $base_dir/build-deps && chmod +x relocate_pcl.sh && ./relocate_pcl.sh
 
-# airsim
-#cd $mavbench_base_dir
-#if [[ ! -d "AirSim" ]];then
-#    cd $mavbench_base_dir/ && git clone https://github.com/hngenc/AirSim.git
-#    cd $mavbench_base_dir/"AirSim" &&\
-#    git fetch origin &&\
-#    git branch future_darwing_dev origin/future_darwing_dev  &&\
-#    git checkout future_darwing_dev
-#fi    
-#
 cd $AirSim_base_dir &&\
     ./setup.sh && \
     ./build.sh
