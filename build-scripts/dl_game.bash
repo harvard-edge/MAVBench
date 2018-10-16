db72702816aae1746d8ee3ae9bf5c604e0ca1a52
zaddan <behzad@gmail.com>
2018-10-16 00:25:16 -0700

    pushing with linux. Windows adds this \r stuff

--- build-scripts/dl_game.bash
+++ build-scripts/dl_game.bash
@@ -2,5 +2,5 @@ fileid="1s9PORdVB8eS1LKyRdyiB6Qr9AP86IfqW"
 filename="games.zip"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null;
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o games.zip
-#unzip games.zip  do not unzip here. do it in windows otherwise it'll error out while running the executable
+# unzip games.zip do not unzip here. do it in windows otherwise it'll error out while running the executable
 
