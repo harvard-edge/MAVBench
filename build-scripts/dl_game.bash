73b4e5ae1b3973cff88512bad64d882242d76c2b
zaddan <behzad@gmail.com>
2018-10-16 15:41:13 -0700

    dl_game and host_root_setup updated so that now the game url is obtained from host_setup_env

--- build-scripts/dl_game.bash
+++ build-scripts/dl_game.bash
@@ -1,4 +1,5 @@
-fileid="1s9PORdVB8eS1LKyRdyiB6Qr9AP86IfqW" #NEED THESE COMMENTS for windows  \r errors
+fileid=$1 #NEED THESE COMMENTS for windows  \r errors
+#fileid="1s9PORdVB8eS1LKyRdyiB6Qr9AP86IfqW" NEED THESE COMMENTS for windows  \r errors
 filename="games.zip" #NEED THESE COMMENTS for windows \r  errors
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null; #NEED THESE COMMENTS for windows \r errors
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o games.zip #NEED THESE COMMENTS for windows \r errors
--- build-scripts/host_root_setup.cmd
+++ build-scripts/host_root_setup.cmd
@@ -1,6 +1,6 @@
 pip install paramiko
 pip install requests
 pip install tqdm
-powershell -command "& { bash dl_game.bash }"
+powershell -command "& { bash dl_game.bash $env:game_fileid}"
 tar -xzf games.zip 
 move games "%base_dir%\test_benches\"
