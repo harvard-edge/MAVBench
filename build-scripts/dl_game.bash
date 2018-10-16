49957394f9e47fd3590fb52a7a290e2177312590
zaddan <behzad@gmail.com>
2018-10-16 00:30:53 -0700

    literal blahs

--- build-scripts/dl_game.bash
+++ build-scripts/dl_game.bash
@@ -1,6 +1,7 @@
-fileid="1s9PORdVB8eS1LKyRdyiB6Qr9AP86IfqW" #blah
-filename="games.zip" #blah
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null; #blah
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o games.zip #blah
+fileid="1s9PORdVB8eS1LKyRdyiB6Qr9AP86IfqW" #NEED THESE COMMENTS for windows \r errors
+filename="games.zip" #NEED THESE COMMENTS for windows \r errors
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null; #NEED THESE COMMENTS for windows \r errors
+
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o games.zip #NEED THESE COMMENTS for windows \r errors
 # unzip games.zip do not unzip here. do it in windows otherwise it'll error out while running the executable
 
