7615e9b41f71dd63aba02f424b312107f40b0ad1
zaddan <behzadboro@gmail.com>
2019-03-13 15:24:43 +0000

    launch files update to reflect the new directory name for mavbench

--- common/control_drone_node.cpp
+++ common/control_drone_node.cpp
@@ -36,16 +36,21 @@ std::string ip_addr__global;
 // *** F:DN main function
 int main(int argc, char **argv)
 {
-    
     // ROS node initialization
     ros::init(argc, argv, "control_drone_node", ros::init_options::NoSigintHandler);
     ros::NodeHandle n;
     signal(SIGINT, sigIntHandler);
  
+    std::string ns = ros::this_node::getName();
     uint16_t port = 41451;
     // ros::param::get("/ip_addr",ip_addr__global);
-    ip_addr__global = "169.254.250.191";
-    ROS_INFO_STREAM("ip to contact to"<<ip_addr__global);
+    if(!ros::param::get("/ip_addr",ip_addr__global)){
+        ROS_FATAL("Could not start exploration. Parameter missing! Looking for %s", 
+                (ns + "/ip_addr").c_str());
+
+    return -1;
+    }
+    ROS_INFO_STREAM("ip to contact to now"<<ip_addr__global);
     Drone drone(ip_addr__global.c_str(), port);
 	ros::Rate pub_rate(5);
 
